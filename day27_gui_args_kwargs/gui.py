c92ad0d46072841b7284459c9778882bada7c61c
aydin <aydintokuslu78@gmail.com>
2022-12-05 12:11:37 +0300

    05.12.22

--- day27_gui_args_kwargs/gui.py
+++ day27_gui_args_kwargs/gui.py
@@ -1,14 +1,33 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 #Label
-my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
-my_label.pack()  # label'ı ekranda ortalamamızı, paketlememizi sağlar.
+my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
+my_label.pack()
+#my_label.pack(side="left")  # label'ı ekranda ortalamamızı, paketlememizi sağlar.
 # pack() fonksiyonu incelenebilir. side = left, right, bottom vb, expand=True,False vb.
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
 
+
+def button_clicked():
+    print("I got clicked")
+    #my_label.config(text="Button Got Clicked")
+    new_text = input.get()
+    my_label.config(text= new_text)
+
+
+# Button
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
+
+# Entry
+input = Entry(width=20)
+input.pack()
+#print(input.get())
 
 
 
